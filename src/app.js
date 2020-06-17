3b7b70b40cef3185ac73a554472109fce49ce9eb
mehrdad-safari <mihrdad.safari@gmail.com>
2020-06-17 14:07:52 +0430

    setting component param

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <WikiApi keyword='Albert_Einstein' locale='en' classname='' />
+      <WikiApi keyword='Albert_Einstein' />
       <Footer />
     </div>
   );
